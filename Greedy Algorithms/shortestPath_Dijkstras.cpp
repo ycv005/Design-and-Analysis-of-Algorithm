211889557c328f56e240a1391722a3cd09b25263
Yash Chandra Verma <ycverma005@gmail.com>
2018-11-02 20:11:12 +0530

    Update shortestPath_Dijkstras.cpp

--- Greedy Algorithms/shortestPath_Dijkstras.cpp
+++ Greedy Algorithms/shortestPath_Dijkstras.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
-#include<limits>    // INT_MAX in this lib, has the infinity value
+#include<limits>
 using namespace std;
 #define v 5
-int choosingV(int dis[],bool setS[]){   //int dis[] is == int *dis
+void relax(int graph[v][v],int u,int j,int *dis,int *isRelax){
+    if(dis[j]>dis[u]+graph[u][j] && isRelax[u]==0){
+        dis[j] = dis[u]+graph[u][j];
+    }  
+}
+int chosingV(int* dis,int* isRelax){
     int max = INT16_MAX,min,set=0;
     for(int i=0;i<v;i++){
-        if(setS[i]==false && dis[i]<max){
+        if(isRelax[i]==0 && dis[i]<max){
             min = dis[i];
             max = dis[i];
             set =i;
@@ -13,41 +18,35 @@ int choosingV(int dis[],bool setS[]){   //int dis[] is == int *dis
     }
     return set;
 }
-void relax(int dis[],int weight,int u,int pos){
-    if (dis[pos]>dis[u]+weight){
-        dis[pos]=dis[u]+weight;
-    }
-}
-void outgoing(int graph[v][v],int dis[],int u){
-    int weight;
+void dijkstra(int graph[v][v],int source){
+    int dis[v],isRelax[v];
     for(int i=0;i<v;i++){
-        if (graph[u][i]>0){
-            weight = graph[u][i];
-            relax(dis,weight,u,i);
+        dis[i]=INT16_MAX;
+        isRelax[i]=0;
+    }    
+    dis[source]=0;
+    for(int i=0;i<v;i++){
+        int u = chosingV(dis,isRelax);
+        for(int j=0;j<v;j++){
+            if(graph[u][j]!=0){
+                relax(graph,u,j,dis,isRelax);
+            }
         }
+        isRelax[u]=1;
     }
-}
-void dijkstra(int graph[v][v],int src){
-    int dis[v]; //
-    bool setS[v];
-    for(int i=0;i<v;i++){
-        dis[i]=INT16_MAX,setS[i]=false;
-    }
-    dis[src]=0;
-    //Now, we are done with the initialization
-    //Now, head towards the  picking the smallest value from the dis array
-    for(int i=0;i<v;i++){
-        int u = choosingV(dis,setS);
-        setS[u]=true;
-        outgoing(graph,dis,u);
-    }
-    // Printing the shortest Distance to all the node from the single source
+
     for(int i=0;i<v;i++){
         cout<<"The Shortest Distance to Vertex "<<i<<" is "<<dis[i]<<endl;
     }
 }
 int main(){
-    int graph[v][v] = {{0, 10, 0, 0, 5},{0, 0, 1, 0, 2},{0, 0, 0, 4, 0},{7, 0, 6, 0, 0},{0, 3, 9, 2, 0}};
+                    //0,1,2,3,4
+    int graph[v][v] = {{0,10,0,0,5},
+                     {0,0,1,0,2},
+                     {0,0,0,4,0},
+                     {7,0,6,0,0},
+                     {0,3,9,2,0}};
+
     dijkstra(graph,0);
     return 0;
 }
